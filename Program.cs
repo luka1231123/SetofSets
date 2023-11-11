7915cacbb36489be9accd4a7f8da11c95f01d8a1
luka1231123 <luka04rekhviashvili@gmail.com>
2023-11-11 15:43:33 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+using System.Linq;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Dynamic;
+using System.Diagnostics.Metrics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SetofSets;
+
+    
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        int c=0;
+        
+        HashSet<int> odd = new HashSet<int>();
+        for(int i=1;i<10000000;i++)
+        {
+            if(i%2==0){
+                odd.Add(2021%(i));
+            }
+            
+            
+        }
+        foreach(int i in odd) 
+        { 
+            //Console.WriteLine(i); 
+            c=c+i;
+        } 
+        Console.WriteLine(c);
+    }
+
+}
